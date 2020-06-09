4367555d008dfad88574486792dfd4461a2ea4da
Tbetcha <troy.boettger@gmail.com>
2020-06-09 18:12:38 -0400

    authenticate users

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -22,7 +22,7 @@ router.get('/', auth, async (req, res) => {
 		res.json(user);
 	} catch (err) {
 		console.error(err.message);
-		res.status(500).send('send seerver error');
+		res.status(500).send('Server error');
 	}
 });
 
