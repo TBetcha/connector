8f72fc63346299e32edacf3cf944fba9b4af36ee
Tbetcha <troy.boettger@gmail.com>
2020-06-14 23:56:42 -0400

    can now add and lookup user posts

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 /** @format */
 
-const express = require('express');
+const express = require('express')
 //bring in the db
-const connectDB = require('./config/db');
+const connectDB = require('./config/db')
 
-const app = express();
+const app = express()
 
-connectDB();
+connectDB()
 
 //Init Middleware
-app.use(express.json({ extended: false }));
+app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => res.send('API running'));
+app.get('/', (req, res) => res.send('API running'))
 
 //define routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/post', require('./routes/api/post'));
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
