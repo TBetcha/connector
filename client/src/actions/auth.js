6dc59141d4396d530f8186591ced7ad2c3a0fc94
Tbetcha <troy.boettger@gmail.com>
2020-06-17 16:12:18 -0400

    e

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,7 +2,10 @@
 
 import axios from 'axios'
 import { setAlert } from './alert'
-import { REGISTER_SUCCESS, REGISTER_FAIL } from './types'
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from './types'
+
+//LOAD USER
+export const loadUser = () => async (dispatch) => {}
 
 //REGISTER USER
 export const register = ({ name, email, password }) => async (dispatch) => {
