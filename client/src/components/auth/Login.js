ff5bbbce9f38834559a6d2701415f1afb25a0fcf
Tbetcha <troy.boettger@gmail.com>
2020-06-18 04:38:38 -0400

    set routing and redirects for login success/failures

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -2,13 +2,12 @@
 /** @format */
 
 import React, { useState, Fragment } from 'react'
-import { useHistory } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { login } from '../../actions/auth'
 
-const Login = ({ login }) => {
+const Login = ({ login, isAuthenticated }) => {
 	const [formData, setFormData] = useState({
 		name: '',
 		email: '',
@@ -25,11 +24,16 @@ const Login = ({ login }) => {
 		login(email, password)
 	}
 
+	//redirect if logged in
+	if (isAuthenticated) {
+		return <Redirect to='/dashboard' />
+	}
+
 	return (
 		<Fragment>
 			{' '}
 			<section className='container'>
-				<h1 className='large text-primary'>Sign Up</h1>
+				<h1 className='large text-primary'>Sign In</h1>
 				<p className='lead'>
 					<i className='fas fa-user'></i> Sign Into Your Account
 				</p>
@@ -65,6 +69,11 @@ const Login = ({ login }) => {
 
 Login.propTypes = {
 	login: PropTypes.func.isRequired,
+	isAuthenticated: PropTypes.bool,
 }
 
-export default connect(null, { login })(Login)
+const mapStateToProps = (state) => ({
+	isAuthenticated: state.auth.isAuthenticated,
+})
+
+export default connect(mapStateToProps, { login })(Login)
