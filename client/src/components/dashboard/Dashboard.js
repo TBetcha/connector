9e7ee4873951af382ec567a2d3f955d34bcebcc4
Tbetcha <troy.boettger@gmail.com>
2020-06-20 18:24:54 -0400

    users profile data is now linked with their log in token

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,12 +1,25 @@
 /** @format */
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { getCurrentProfile } from '../../actions/profile'
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+	useEffect(() => {
+		getCurrentProfile()
+	}, [])
 
-const dashboard = (props) => {
-	return <div>dashboard</div>
+	return <div>Dashboard</div>
 }
 
-dashboard.propTypes = {}
+Dashboard.propTypes = {
+	getCurrentProfile: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
+	profile: PropTypes.object.isRequired,
+}
+const mapStateToProps = (state) => ({
+	auth: state.auth,
+	profile: state.profile,
+})
 
-export default dashboard
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard)
